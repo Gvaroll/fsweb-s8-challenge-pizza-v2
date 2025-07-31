795f9fe5420e7c2a31b04cdfde33d4a2941bb14b
Gvaroll <gorkemvarol2@gmail.com>
2025-07-31 18:28:20 +0300

    back to local

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fsweb-s8-challenge-pizza-v2/',
   plugins: [react()],
-})
+});
